499f83d6f05801f0e262e1f98c9d38061a821cbe
Srinivas <54628670+Srini-max@users.noreply.github.com>
2020-05-12 12:19:48 +0400

    Initial commit

---
+++ src/ios/FirebaseAnalyticsPlugin.m
@@ -0,0 +1,81 @@
+#import "FirebaseAnalyticsPlugin.h"
+
+@import Firebase;
+
+
+@implementation FirebaseAnalyticsPlugin
+
+- (void)pluginInitialize {
+    NSLog(@"Starting Firebase Analytics plugin");
+
+    if(![FIRApp defaultApp]) {
+        [FIRApp configure];
+    }
+}
+
+- (void)logEvent:(CDVInvokedUrlCommand *)command {
+    NSString* name = [command.arguments objectAtIndex:0];
+    NSDictionary* parameters = [command.arguments objectAtIndex:1];
+    /*
+    if([name isEqualToString:@"sign_up"]) {
+        [FIRAnalytics logEventWithName:kFIREventSignUp parameters:@{
+            kFIRParameterSignUpMethod: [parameters valueForKey:@"method"]
+        }];
+    } else if([name isEqualToString:@"login"]) {
+        [FIRAnalytics logEventWithName:kFIREventLogin parameters:@{
+            kFIRParameterMethod: [parameters valueForKey:@"method"]
+        }];
+    } else {
+        [FIRAnalytics logEventWithName:name parameters:parameters];
+    }
+    */
+    [FIRAnalytics logEventWithName:name parameters:parameters];
+    CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+}
+
+- (void)setUserId:(CDVInvokedUrlCommand *)command {
+    NSString* id = [command.arguments objectAtIndex:0];
+
+    [FIRAnalytics setUserID:id];
+
+    CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+}
+
+- (void)setUserProperty:(CDVInvokedUrlCommand *)command {
+    NSString* name = [command.arguments objectAtIndex:0];
+    NSString* value = [command.arguments objectAtIndex:1];
+
+    [FIRAnalytics setUserPropertyString:value forName:name];
+
+    CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+}
+
+- (void)setEnabled:(CDVInvokedUrlCommand *)command {
+    bool enabled = [[command.arguments objectAtIndex:0] boolValue];
+
+    [FIRAnalytics setAnalyticsCollectionEnabled:enabled];
+
+    CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+}
+
+- (void)setCurrentScreen:(CDVInvokedUrlCommand *)command {
+    NSString* name = [command.arguments objectAtIndex:0];
+
+    [FIRAnalytics setScreenName:name screenClass:nil];
+
+    CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+}
+
+- (void)resetAnalyticsData:(CDVInvokedUrlCommand *)command {
+    [FIRAnalytics resetAnalyticsData];
+
+    CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+}
+
+@end
